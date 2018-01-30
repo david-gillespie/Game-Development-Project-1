8866f149b9b66c874031342229773edcb68beac4
bgoff1 <35714383+bgoff1@users.noreply.github.com>
2018-01-29 20:26:53 -0600

    Added static Maze Scene
    Added a Maze Scene, contains a ball that rolls with a timer attachment, Launch Pads that make the player jump if they move onto it, and teleporter pads that swap the players position with the corresponding pad.

---
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,156 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class PlayerController : MonoBehaviour {
+
+    public Text winText;
+    public Button newGameButton;
+    public Rigidbody player;
+
+    private bool canMove;
+    private bool gameOver;
+    private float speed = 10;
+    private GameObject[] pickups;
+    private Vector3 startPostion = new Vector3(-68, 0.5f, -68);
+    private Vector3 launchPower = new Vector3(0, 25, 0);
+    private Vector3 jump = new Vector3(0, 10, 0);
+
+    void Start ()
+    {
+        newGameButton.onClick.AddListener(startNewGame);
+        startNewGame();
+    }
+	
+    public void startNewGame()
+    {
+        gameOver = false;
+        resumeGame();
+
+        pickups = Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[];
+        foreach (GameObject g in pickups)
+        {
+            if (g.CompareTag("Launch Pad"))
+                g.SetActive(true);
+        }
+        player.MovePosition(startPostion);
+        player.Sleep();
+        player.WakeUp();
+    }
+
+
+	private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (winText.text != "You Win!" && winText.text != "You Lose.")
+            {
+                if (canMove)
+                    showMenu();
+                else
+                    resumeGame();
+            }
+            else
+            {
+                if (newGameButton.gameObject.activeSelf)
+                    closeMenu();
+                showMenu();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            if (player.position.y == 0.5 && winText.text == "")
+            player.AddForce(jump * speed);
+	}
+
+    private void closeMenu()
+    {
+        newGameButton.gameObject.SetActive(false);
+    }
+
+    private void showMenu()
+    {
+        canMove = false;
+        newGameButton.gameObject.SetActive(true);
+        player.Sleep();
+        player.WakeUp();
+    }
+
+    private void resumeGame()
+    {
+        canMove = true;
+        newGameButton.gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (canMove)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+            player.AddForce(movement * speed);
+        }
+        if (Math.Abs(player.position.x) > 71 || Math.Abs(player.position.z) > 71)
+        {
+            canMove = false;
+            player.Sleep();
+            player.WakeUp();
+            player.MovePosition(startPostion);
+            gameOver = true;
+            winText.text = "You Lose.";
+            showMenu();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!gameOver && !newGameButton.IsActive())
+        {
+            if (other.gameObject.CompareTag("Win Zone"))
+            {
+                player.MovePosition(startPostion);
+                winText.text = "You Win!";
+                player.Sleep();
+                player.WakeUp();
+                canMove = false;
+            }
+            if (other.gameObject.CompareTag("Launch Pad"))
+            {
+                other.gameObject.SetActive(false);
+                player.AddForce(launchPower * speed);
+            }
+            if (other.gameObject.CompareTag("Teleporter"))
+            {
+                teleport(other, "Teleporter 2");
+            }
+            if (other.gameObject.CompareTag("Teleporter 2"))
+            {
+                teleport(other, "Teleporter");
+            }
+        }
+    }
+
+    private void teleport(Collider other, string tagToMatch)
+    {
+        other.gameObject.SetActive(false);
+        Vector3 tpLoc = jump;
+        pickups = Resources.FindObjectsOfTypeAll(typeof(GameObject)) as GameObject[];
+        foreach (GameObject go in pickups)
+        {
+            if (go.gameObject.CompareTag(tagToMatch))
+            {
+                go.SetActive(false);
+                tpLoc = go.transform.position;
+                break;
+            }
+        }
+        if (tpLoc != jump)
+        {
+            player.MovePosition(tpLoc);
+        }
+    }
+}
---
+++ Maze/Assets/Scripts/Timer.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    public Text pauseText;
+    public Button newGameButton;
+    public Rigidbody pc;
+
+    private Vector3 startPosition = new Vector3(-68, 0.5f, -68);
+    private bool isPaused;
+    private int maxSeconds = 120;
+    private float elapsedTime;
+	void Start () {
+        newGameButton.onClick.AddListener(startNewGame);
+        startNewGame();
+        newGameButton.gameObject.SetActive(true);
+	}
+	
+    public void startNewGame()
+    {
+        elapsedTime = 0;
+        isPaused = false;
+        if (pauseText.text != "You Win!" || pauseText.text != "You Lose.")
+            pauseText.text = "";
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (!isPaused)
+        {
+            if (pauseText.text != "You Win!" && pauseText.text != "You Lose.")
+            {
+                string seconds = "";
+                elapsedTime += Time.deltaTime;
+                float timeLeft = maxSeconds - elapsedTime;
+                if (timeLeft % 60 < 0)
+                {
+                    pauseText.text = "You Lose.";
+                    pc.MovePosition(startPosition);
+                    pc.Sleep();
+                    pc.WakeUp();
+                }
+                else
+                {
+                    seconds = timeLeft.ToString("f1");
+                }
+                timerText.text = seconds;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (pauseText.text != "You Lose." && pauseText.text != "You Win!"
+                && pauseText.text != "Paused")
+                pauseText.text = "Paused";
+            else if (pauseText.text == "Paused")
+                pauseText.text = "";
+        }
+
+    }
+}
