139c4a81a9926057ab5c77d40323c9d2b6e2d148
dgillespie <dgillespie@harding.edu>
2018-02-03 13:11:17 -0600

    Improved minimap
    Zoomed the minimap in, so you can't see the whole maze, and can actually see the player, and adjusted it to where it will always be a square instead of a rectangle.

---
+++ Maze/Assets/Scripts/NewMazeScripts/MiniMapController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset = new Vector3(0,20,0);
+	private Rect viewport;
+
+	void Start () {
+		Camera cam = GameObject.Find ("MiniMapCamera").GetComponent<Camera>();
+		float aspectRatio = Screen.width / Screen.height;
+		cam.rect = new Rect (1.0f-(0.3f/aspectRatio)-0.03f,0.67f,0.3f/aspectRatio,0.3f);
+	}
+
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+
+}
