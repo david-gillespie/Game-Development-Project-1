429d434f7e8236b6ccf0510f3126db06c8d3801d
dgillespie <dgillespie@harding.edu>
2018-02-10 17:27:05 -0600

    Basic coin animation
    -Added basic coin animation to the jump level.

---
+++ Maze/Assets/Scripts/JumpScripts/CoinController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	private Vector3 startingPos;
+	private Vector3 rotate;
+	private float bounce = 0.2f;
+	private float speed = 0.005f;
+	private int directionUp = 1;
+
+	void Start () {
+		startingPos = transform.position;
+		if (Random.Range (0, 2) == 1) {
+			directionUp = 1;
+		} else {
+			directionUp = -1;
+		}
+		rotate = new Vector3 (0,1,0);
+	}
+	
+	void Update () {
+		if (startingPos.y + bounce < transform.position.y) {
+			directionUp = -1;
+		}else if(startingPos.y - bounce > transform.position.y){
+			directionUp = 1;
+		}
+		transform.position =new Vector3 (transform.position.x,transform.position.y+(speed*directionUp),transform.position.z);
+		transform.Rotate (0, 0, Time.deltaTime*30); //Rotates 30 degrees a second
+	}
+}
